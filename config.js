b565275099f86f450c8f872ea828f2cdd17f9dc2
mayank.singh <mayank.singh@intouch.rocks>
2022-03-14 00:22:09 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteTitle: 'Atulya Organics', // <title>
+  manifestName: 'Atulya',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/atulya-organics/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'ATULYA ORGANICS',
+  subHeading: 'Natural Traditional and Sustainable',
+};
